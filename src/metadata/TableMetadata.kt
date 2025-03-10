1fa9b573757c8817716816fc701c8b093138ef85
GiJungPark <weeds2577@gmail.com>
2025-03-10 15:38:54 +0900

    feat: 스키마의 모든 테이블 및 메타데이터를 불러오는 로직 작성

--- src/metadata/TableMetadata.kt
+++ src/metadata/TableMetadata.kt
@@ -1,12 +1,15 @@
 package metadata
 
-data class TableMetadata (
-    val tableSchema: String,
-    val tableName: String,
-    val columnName: String,
-    val columnType: String?,
-    val isNullable: String,
-    val defaultValue: String?
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class TableMetadata @JsonCreator constructor(
+    @JsonProperty("TABLE_SCHEMA") val tableSchema: String,
+    @JsonProperty("TABLE_NAME") val tableName: String,
+    @JsonProperty("COLUMN_NAME") val columnName: String,
+    @JsonProperty("DATA_TYPE") val columnType: String?,
+    @JsonProperty("IS_NULLABLE") val isNullable: String,
+    @JsonProperty("COLUMN_DEFAULT") val defaultValue: String?
 ) {
     override fun toString(): String {
         return "Table: $tableName, Column: $columnName, Data Type: $columnType, Nullable: $isNullable, Default Value : $defaultValue"
